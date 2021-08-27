c982847e5aa8103e743db11f8b4f3119832c97bc
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-08-27 18:25:02 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool first(int);
+
+int main()
+{
+	int x;
+
+	cout << "Press number: ";
+
+	cin >> x;
+
+	if (first(x) && x != 1)
+	{
+		cout << "True number!";
+	}
+	else cout << "False number!";
+}
+
+bool first(int y)
+{
+	int i;
+
+	bool z = true;
+
+	for (i = 2; i <= sqrt(abs(y)); i++)
+	{
+		if (y % i == 0)
+		{
+			z = false;
+			break;
+		}
+	}
+
+	return z;
+}
